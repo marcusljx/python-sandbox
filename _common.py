37f5c5bea9cda87b53690bb061ab867ac4b98cc4
marcusljx <marcusljx@gmail.com>
2016-09-05 08:25:58 +0800

    Added Terminal class

---
+++ _common.py
@@ -0,0 +1,5 @@
+import datetime
+
+
+def timestamp():
+    return datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d-%H%M%S.%f")
---
+++ os_tools/Terminal.py
@@ -0,0 +1,36 @@
+""" Emulates the terminal
+"""
+import subprocess
+import logging
+import sys
+
+import _common
+
+
+class Terminal:
+    """ Terminal class for instantiating commands in a subprocess"""
+    def __init__(self, name="Terminal"):
+        self.logger = logging.getLogger(name)
+
+    def runCommand(self, commandString):
+        self.logger.info("{}::{}".format(_common.timestamp(), commandString))
+
+        try:
+            p = subprocess.Popen(commandString, stdout=subprocess.PIPE, bufsize=1)
+            with p.stdout:
+                for line in iter(p.stdout.readline, b''):
+                    sys.stdout.write(line)
+                    sys.stdout.flush()
+                    self.logger.log(self.logger.level, line)
+            status = p.wait()
+
+        except subprocess.CalledProcessError:
+            self.logger.error("Command {", commandString, "} returned non-zero exit status.")
+            raise
+
+        except OSError:
+            cmd = commandString.split(' ').pop()
+            self.logger.error("Command not found: ", cmd)
+            raise
+
+        return status
